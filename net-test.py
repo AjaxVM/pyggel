9bc2bc024958d0d057000e1625a6af4485bb2daa
AjaxVM <roebros@gmail.com>
2018-09-06 09:16:54 -0500

    better system for identifying connections

--- net-test.py
+++ net-test.py
@@ -11,19 +11,6 @@ class MyHandler(handler.Handler):
         self.server = server
         self.client = client
 
-    # TODO: need a way to differentiate connections as being client or server or something...
-    # maybe connection label as a param to the factory/start_server/connect?
-    # that seems to work, but maybe not quite what we want
-
-    # TODO: maybe make events different depending on the side, ie
-    #       server:
-    #           connection.listening, connection.new, connection.message, connection.lost_client
-    #       client:
-    #           connection.connected, connection.disconnected, connection.message
-
-    # actually this is weird, we get connection.new on the server, but the connection.is_server is True, which is wrong
-    # it's more that the handler is the server but it is the connection to the client
-
     def label(self, evt):
         if evt.connection.listener == self.server:
             return 'server'
@@ -41,12 +28,12 @@ class MyHandler(handler.Handler):
     @handler.register('connection.message')
     def message(self, evt):
         print('message', self.label(evt), evt.message)
-        evt.connection.close()
-        # evt.connection.transport.write_eof()
+        if not evt.connection.is_server:
+            evt.connection.send('got it, goodbye')
+            evt.connection.close()
 
 
 def main():
-
     my_loop = loop.AsyncLoop()
 
     server_listener = net2.start_server()
